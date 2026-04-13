04bf906cc4eb3adaf86113e10d7295f6956c1706
Nomen-collab <9988@holbertonstudents.com>
2026-04-13 22:09:18 +0200

    Task 8

--- linux_security/1x00_linux_fundamentals/8-log_policy.sh
+++ linux_security/1x00_linux_fundamentals/8-log_policy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chown root:$2 $1 && chmod 2750 $1 && printf '/var/log/app/*.log {\n    create 0640 root '$2'\n}\n' > /etc/logrotate.d/app
+chown root:$2 $1 && chmod 2750 $1 && printf "/var/log/app/*.log {\n    create 0640 root $2\n}\n" > /etc/logrotate.d/app
