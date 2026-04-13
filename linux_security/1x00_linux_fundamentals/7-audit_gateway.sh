99bd1f1464618828e4130e242fcfcb2453736e29
Nomen-collab <9988@holbertonstudents.com>
2026-04-13 21:37:52 +0200

    Task 7

--- linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
+++ linux_security/1x00_linux_fundamentals/7-audit_gateway.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '#!/bin/bash' > /usr/local/bin/audit-read-secret && echo 'cat /var/www/html/secret_config.php' >> /usr/local/bin/audit-read-secret && chmod +x /usr/local/bin/audit-read-secret && echo "$1 ALL=(ALL) NOPASSWD: /usr/local/bin/audit-read-secret" > /etc/sudoers.d/audit-gateway
+echo '#!/bin/bash' > /usr/local/bin/audit-read-secret && echo 'cat /var/www/html/secret_config.php' >> /usr/local/bin/audit-read-secret && chmod +x /usr/local/bin/audit-read-secret && echo "$1 ALL=(root) NOPASSWD: /usr/local/bin/audit-read-secret" > /etc/sudoers.d/audit-gateway
