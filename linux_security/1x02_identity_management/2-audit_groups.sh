e482e316709e6b2e41dca50bd5032b0b4ad0475e
Nomen-collab <9988@holbertonstudents.com>
2026-04-14 13:46:49 +0200

    Task 2

--- linux_security/1x02_identity_management/2-audit_groups.sh
+++ linux_security/1x02_identity_management/2-audit_groups.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-while IFS=: read -r user _ uid _; do
-    if [ "$uid" -ge 1000 ]; then
-        for group in disk docker shadow; do
-            if id -nG "$user" 2>/dev/null | grep -qw "$group"; then
-                echo "$user:$group"
-            fi
-        done
-    fi
-done < "$1"
+awk -F: '$3>=1000 {print $1}' "$1" | while read -r user; do
+    for group in disk docker shadow; do
+        if id -nG "$user" 2>/dev/null | grep -qw "$group"; then
+            echo "$user:$group"
+        fi
+    done
+done
