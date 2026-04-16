0caa1772a302ceb47589d36975679be982697a1b
Nomen-collab <9988@holbertonstudents.com>
2026-04-17 01:38:40 +0200

    Task 7

--- linux_security/1x02_identity_management/7-sudo_config.sh
+++ linux_security/1x02_identity_management/7-sudo_config.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "$1 ALL=(ALL) /usr/bin/systemctl restart apache2, /usr/bin/journalctl" > /etc/sudoers.d/junior && visudo -c -f /etc/sudoers.d/junior
+echo "$1 ALL=(ALL) /usr/bin/systemctl restart apache2, /usr/bin/journalctl" > /etc/sudoers.d/junior && chmod 440 /etc/sudoers.d/junior && visudo -c -f /etc/sudoers.d/junior
