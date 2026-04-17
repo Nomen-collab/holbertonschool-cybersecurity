2f6ebf8da5e414fdb92464dbe6d49b1d84fd9659
Nomen-collab <9988@holbertonstudents.com>
2026-04-17 11:21:37 +0200

    Task 10

--- linux_security/1x03_system_visibility/10-recent_logs.sh
+++ linux_security/1x03_system_visibility/10-recent_logs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-awk -v date="$(date --date='30 minutes ago' '+%b %d %H:%M:%S')" '$0 > date && /sshd/ {print}' /var/log/auth.log 2>/dev/null
+awk -v date="$(date --date='30 minutes ago' '+%b %d %H:%M:%S')" '$0 > date && /sshd/ {print}' "${1:-/var/log/auth.log}" 2>/dev/null
