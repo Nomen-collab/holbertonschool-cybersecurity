16ddc2d36a51440c6fb6d74e4953349722902b52
Nomen-collab <9988@holbertonstudents.com>
2026-04-17 11:45:40 +0200

    Task 8

--- linux_security/1x03_system_visibility/8-who_listens.sh
+++ linux_security/1x03_system_visibility/8-who_listens.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ss -tulpn 2>/dev/null | grep ":${1}\ " | grep -E "LISTEN|LISTENING" | awk '{print $7}' | cut -d'"' -f2 | cut -d',' -f1 | head -1
+ss -tulpn 2>/dev/null | grep ":${1} " | grep LISTEN | awk -F'"' '{print $2}'
